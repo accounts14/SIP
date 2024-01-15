77646b7e37506a57da9c40fd6f7c0cdbeebf8194
nurkhamid22 <nurkhamid22@gmail.com>
2024-01-15 14:44:21 +0700

    fix shcool seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class,
+            SchoolLevelSeeder::class,
             SchoolTableSeeder::class,
         ]);
     }
